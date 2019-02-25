83eaabb84d42aad4c907699f4f9d0768e2f43a83
Musa <musaugurlu@users.noreply.github.com>
2019-02-24 20:16:29 -0500

    Fix Build.ps1 #3

--- Build.ps1
+++ Build.ps1
@@ -3,6 +3,6 @@ Install-WindowsFeature AD-Domain-Services -Restart -Confirm:$false
 Import-Module ActiveDirectory
 Import-Module ADDSDeployment
 
-Install-ADDSForest -CreateDnsDelegation:$false -DomainName “musaugurlu.com” -DomainNetbiosName “MUSAUGURLU” -SafeModeAdministratorPassword (ConvertTo-SecureString "Sup3rS3cureP@ssw0rd" -AsPlainText -Force) -InstallDns:$true  -NoRebootOnCompletion:$True -Force:$true
+Install-ADDSForest -CreateDnsDelegation:$false -DomainName "musaugurlu.com" -DomainNetbiosName "MUSAUGURLU" -SafeModeAdministratorPassword (ConvertTo-SecureString "Sup3rS3cureP@ssw0rd" -AsPlainText -Force) -InstallDns:$true  -NoRebootOnCompletion:$True -Force:$true
 
 Get-Forest
